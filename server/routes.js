2ffa38af7775cff865518cfacd182b1ef3baf269
Artur Gajewski <info@arturgajewski.com>
2017-04-03 12:18:45 +0300

    Empty line breaks removed

--- server/routes.js
+++ server/routes.js
@@ -3,12 +3,10 @@ var router = express.Router();
 
 var db = require('./db');
 
-
 router.get('/api/sensors', db.getAllSensors);
 router.get('/api/sensors/:id', db.getSingleSensor);
 router.post('/api/sensors', db.createSensor);
 router.put('/api/sensors/:id', db.updateSensor);
 router.delete('/api/sensors/:id', db.removeSensor);
-
 
 module.exports = router;
